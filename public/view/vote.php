9484ea43d4cbf3df85ee2689eb12be7a34ed5def
stasonli <stasonli29@gmail.com>
2020-03-31 11:51:46 +0900

    created voting page w. basic style

---
+++ public/view/vote.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Vote</title>
+        <link rel="stylesheet" href="../styles/style.css"/>
+    </head> 
+    <body>
+        <div id="buttons">
+            <form method="POST" action="index.php">
+                <input type="hidden" name="action" value="makeAVote"/>
+                <input type="submit" name="a" value="A" id="aBtn"/>
+                <input type="submit" name="b" value="B" id="bBtn"/>
+            </form>
+        </div>
+    </body>
+</html>
