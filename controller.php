51ce887cbe96cfc730761c65ed56a5fefff93bae
thatguy1155 <jamesglass@Jamess-MacBook-Pro.local>
2020-03-31 11:22:37 +0900

    mvc complete

--- controller.php
+++ controller.php
@@ -1,1 +1,19 @@
-<p>test</p>
+<?php
+
+
+
+
+function login() {
+        require("view/login.php");  //change to pg 1 page name
+    }
+
+
+function vote() {
+    require("view/vote.php");   //change to page 2 page name
+}
+
+function admin() {
+    require("view/admin.php");   //change to page 3 page name
+}
+
+
