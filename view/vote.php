a76c434ebe6ddfae6fd9d27dca90ac044614b6e6
stasonli <stasonli29@gmail.com>
2020-03-31 16:32:20 +0900

    changed styling a little after merge with Eunhye

--- view/vote.php
+++ view/vote.php
@@ -8,8 +8,7 @@
     </head> 
     <body>
         <div id="buttons">
-            <form method="POST" action="../index.php" id="voteForm">
-                <input type="hidden" name="voted" value="true"/>
+            <form method="POST" action="index.php" id="voteForm">
                 <input type="hidden" name="action" value="vote"/>
                 <input type="hidden" name="userId" value=<?= $_COOKIE['userId']?> />
                 <input type="submit" name="a" value="a" id="aBtn"/>
