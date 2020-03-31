ed246f2aa2db2682983a916a7943004ca585f1c9
kimeunhye224 <kimeunhye224@gmail.com>
2020-03-31 13:28:07 +0900

    -register page working with cookie set for userId -rearranged some folders/files (added model and controller folders)

--- view/register.php
+++ view/register.php
@@ -4,18 +4,20 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register</title>
-        <link rel="stylesheet" href="public/css/style.css"/>
-        <link rel="stylesheet" type="text/css" href="public/css/mobileScreen.css">
+        <link rel="stylesheet" href="public/styles/style.css"/>
+        <link rel="stylesheet" type="text/css" href="public/styles/mobileScreen.css">
     </head> 
     <body>
-        <div id="formWrapper">
-            <form method="post" action="index.php">
-                <input type="hidden" name="action" value="vote">
+        <div id="registerFormWrapper">
+            <form method="post" action="./index.php">
+                <input type="hidden" name="action" value="register">
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name">
                 <label for="email">Email</label>
                 <input type="text" name="email" id="email">
+                <input type="submit" name="register" id="register" value="Register">
             </form>
         </div>
     </body>
 </html>
+
