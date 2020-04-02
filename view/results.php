4c9fa12cb721fbf6e0abe21bd4bdc3462fa5adb7
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-04-02 09:14:06 +0900

    results page style

--- view/results.php
+++ view/results.php
@@ -1,15 +1,12 @@
-<span>Results of the Vote</span>
+<body id="results">
+    <link rel="stylesheet" href="../public/styles/style.css" rel="stylesheet"/>
+        <span id="resultText">Results of the Vote</span>
 
-<link rel="stylesheet" href="../public/styles/style.css" rel="stylesheet"/>
+            <div id="resultContainer">             
+                <div id="resultB" class="score"></div>
+                <div id="resultA" class="score"></div>               
+            </div>
 
-
-<div id="resultContainer">
-
-    <div id="resultB"></div>
-    <div id="resultA"></div>
-    
-</div>
-
-<script src="../public/js/results.js"></script>
-
-<button id="do">do shit</button>
+            <!-- <button id="do">do shit</button> -->
+    <script src="../public/js/results.js"></script>
+</body>
