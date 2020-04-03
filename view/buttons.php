d6cf0586c292614d5edbfffa21103a382c024e7d
kimeunhye224 <kimeunhye224@gmail.com>
2020-04-03 15:01:54 +0900

    added options/question basic

--- view/buttons.php
+++ view/buttons.php
@@ -2,7 +2,10 @@
     <form method="POST" action="index.php" id="voteForm">
         <input type="hidden" name="action" value="vote"/>
         <input type="hidden" name="userId" value=<?= $_COOKIE['userId']?> />
+        <p class="question">Question</p>
+        <p class="option" id="optionA">optionA</p>
         <input type="submit" name="a" value="a" id="aBtn"/>
+        <p class="option" id="optionB">optionB</p>
         <input type="submit" name="b" value="b" id="bBtn"/>
     </form>
 </div>
--- view/results.php
+++ view/results.php
@@ -1,12 +1,12 @@
 <body id="results">
     <link rel="stylesheet" href="public/styles/style.css" rel="stylesheet"/>
         <!-- <span id="resultText">Results of the Vote</span> -->
-
-            <div id="resultContainer">             
-                <div id="resultA" class="score"></div>   
-                <div id="resultB" class="score"></div>            
-            </div>
-
-            <!-- <button id="do">do shit</button> -->
+        <p class="question">Question</p>
+        <div id="resultContainer">
+            <p class="option" id="optionA">optionA</p>
+            <div id="resultA" class="score"></div>   
+            <p class="option" id="optionB">optionB</p>
+            <div id="resultB" class="score"></div>           
+        </div>
     <script src="public/js/results.js"></script>
 </body>
